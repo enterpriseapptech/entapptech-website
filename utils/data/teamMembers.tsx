cec5990f0cf3c39613371b3c4c3d8299baddd743
enterpriseapptech <enterpriseapptech@gmail.com>
2025-08-14 17:17:31 +0100

    Update teamMembers.tsx

--- utils/data/teamMembers.tsx
+++ utils/data/teamMembers.tsx
@@ -4,37 +4,45 @@ export const teamMembers = [
       name: "Isaac Adejo Onekutu",
       role: "Founder & CEO",
       image: "/images/ceo.png", 
+      description: "Isaac A. Onekutu is a skilled DevOps Engineer with a strong background in automating CI/CD pipelines, containerization, and Infrastructure as Code (IaC) across cloud environments. He is proficient in scripting, Linux administration, and using monitoring tools to enhance system reliability.
+Isaac has held key roles at Enterprise Application Tech LLC, Harmonia Holdings Group LLC (IRS project), and Dominion System Inc., where he led critical infrastructure projects, including cloud migrations and AWS automation that significantly reduced deployment failures.
+He holds a Master of Science in Applied Artificial Intelligence from Lipscomb University, a Graduate Certificate in DevOps & Cloud  Engineering from Purdue University, Master of Science in Human Resources Leadership from Middle Tennessee State University, and an MBA from the University of Calabar—combining technical expertise with strong leadership and strategic insight.
+", 
     },
     {
       id: 2,
       name: "Mrs. Grace Unekwu Onekutu",
       role: "Vice President Operations",
       image: "/images/team-2.png", 
+      description: "", 
     },
     {
       id: 3,
       name: "Barrister Maji",
       role: "Legal Adviser",
       image: "/images/team-3.png", 
+      description: "", 
     },
     {
       id: 4,
       name: "Joshua Obaje",
-      role: "Frontend Developer",
+      role: "Sr. Full Stack Developer - (Remote)",
       image: "/images/team-4.png", 
-
+      description: "", 
     },
     {
       id: 5,
       name: "Emmanuel Kachikwu",
       role: "UX Engineer",
-      image: "/images/image-2.png", 
+      image: "/images/image-2.png",
+      description: "", 
     },
     {
       id: 6,
       name: "Virtue",
       role: "Backend Developer",
       image: "/images/image-2.png", 
+      description: "", 
     },
    
   ];
