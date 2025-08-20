a2ed8c944bf57cd6e55058d63c10348d53882184
enterpriseapptech <enterpriseapptech@gmail.com>
2025-08-20 16:50:49 +0100

    Update teamMembers.tsx

--- utils/data/teamMembers.tsx
+++ utils/data/teamMembers.tsx
@@ -4,10 +4,10 @@ export const teamMembers = [
       name: "Isaac Adejo Onekutu",
       role: "Founder & CEO",
       image: "/images/ceo.png", 
-      description: "Isaac A. Onekutu is a skilled DevOps Engineer with a strong background in automating CI/CD pipelines, containerization, and Infrastructure as Code (IaC) across cloud environments. He is proficient in scripting, Linux administration, and using monitoring tools to enhance system reliability.
+      description: `Isaac A. Onekutu is a skilled DevOps Engineer with a strong background in automating CI/CD pipelines, containerization, and Infrastructure as Code (IaC) across cloud environments. He is proficient in scripting, Linux administration, and using monitoring tools to enhance system reliability.
 Isaac has held key roles at Enterprise Application Tech LLC, Harmonia Holdings Group LLC (IRS project), and Dominion System Inc., where he led critical infrastructure projects, including cloud migrations and AWS automation that significantly reduced deployment failures.
 He holds a Master of Science in Applied Artificial Intelligence from Lipscomb University, a Graduate Certificate in DevOps & Cloud  Engineering from Purdue University, Master of Science in Human Resources Leadership from Middle Tennessee State University, and an MBA from the University of Calabar—combining technical expertise with strong leadership and strategic insight.
-", 
+`, 
     },
     {
       id: 2,
